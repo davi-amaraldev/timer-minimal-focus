7c327b5266a045ce28e8cdb433fcc8887acd8125
Davi Amaral <imyuno2022@outlook.com>
2026-03-10 21:51:06 -0300

    fix(app): corrigindo o app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,24 @@
 import { saveState, loadState } from "./storage.js";   
 import { start, pause, reset, tick } from "./timer.js";
 
-const state = loadState() || saveState();
+const state = loadState();
+
+function syncState(){
+    saveState(state);
+}
+
+function startHandler(){
+    start(state);
+    syncState();
+}
+
+function pauseHandler(){
+    pause(state);
+    syncState();
+}
+
+function resetHandler(){
+    reset(state);
+    syncState();
+}
+
